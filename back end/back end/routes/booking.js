915c4647a0d9ed8479cba7b2fd33089de9f2aa47
hung2407 <manhhung270702@gmail.com>
2025-03-25 10:24:40 +0700

    Delete back end/back end directory

--- back end/back end/routes/booking.js
+++
@@ -1,8 +0,0 @@
-// routes/booking.js
-const express = require('express');
-const router = express.Router();
-const { createBooking } = require('../controllers/bookingController');
-
-router.post('/', createBooking);
-
-module.exports = router; // Export router trực tiếp
